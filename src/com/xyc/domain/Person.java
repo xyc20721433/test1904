fef4580d2383ae22afd03761ad08694ed785ea92
xyc <2352538576@qq.com>
2021-09-27 20:38:14 +0800

    sd

---
+++ src/com/xyc/domain/Person.java
@@ -0,0 +1,9 @@
+package com.xyc.domain;
+
+/**
+ * @author xyc
+ * @create 2021-09-27-20:15
+ */
+public class Person {
+    private int age;
+}
---
+++ src/com/xyc/domain/Student.java
@@ -0,0 +1,9 @@
+package com.xyc.domain;
+
+/**
+ * @author xyc
+ * @create 2021-09-27-20:17
+ */
+public class Student extends Person {
+    private String stuId;
+}
